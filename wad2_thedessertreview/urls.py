43c12a2408d5b8944cc3010c36f9cfa4ad1cd888
Cheuk Yeung Sophia Chung <2572284c@student.gla.ac.uk>
2021-03-12 23:05:20 +0800

    shortened folder name

--- wad2_thedessertreview/urls.py
+++ wad2_thedessertreview/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from dessertreview import views
+from dr import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('dessertreview/', include('dessertreview.urls')),
+    path('dessertreview/', include('dr.urls')),
     path('admin/', admin.site.urls),
 ]
