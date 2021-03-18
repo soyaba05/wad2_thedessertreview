d3a26407e43cb0263f1856073f05104dd2114b3d
Robbie Edgar <2461502E@student.gla.ac.uk>
2021-03-18 17:06:59 +0000

    Created models and added to admin panel

--- dessertreview/admin.py
+++ dessertreview/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from dessertreview.models import Shop, Dessert, UserProfile, Review
 # Register your models here.
+class ShopAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Shop, ShopAdmin)
+admin.site.register(Dessert)
+admin.site.register(UserProfile)
+admin.site.register(Review)
--- dessertreview/models.py
+++ dessertreview/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Shop(models.Model):
+    NAME_MAX_LENGTH = 128
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    location = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Shop, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural='Dessert Shops'
+
+    def __str__(self):
+        return self.name
+
+class Dessert(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=TITLE_MAX_LENGTH)
+    url = models.URLField(max_length=URL_MAX_LENGTH)
+    picture = models.ImageField(upload_to='dessert_images', blank=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # The additional attributes we wish to include.
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Review(models.Model):
+    REVIEW_MAX_LENGTH = 250
+
+    dessert = models.ForeignKey(Dessert, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    text = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.text
+
