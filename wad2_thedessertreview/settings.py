99c6b33ab5f73ef43e59031baa485f9a0d6c97e9
Soyaba Hasan <hasansoyaba05@gmail.com>
2021-03-24 17:19:50 +0000

    css style works now

--- wad2_thedessertreview/settings.py
+++ wad2_thedessertreview/settings.py
@@ -122,8 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'wad2_thedessertreview/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [ STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
