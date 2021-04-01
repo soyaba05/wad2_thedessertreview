33d9bf954e6f478c26fb6ee512dc59b2adcc5169
Cheuk Yeung Sophia Chung <2572284c@student.gla.ac.uk>
2021-04-01 21:49:15 +0800

    changes to footer

--- static/js/home.js
+++ static/js/home.js
@@ -7,7 +7,7 @@ var allImages = "";
 for (var i = 0; i < 25; i++) {
     var width = getRandomSize(200, 400);
     var height =  getRandomSize(200, 400);
-    allImages += '<img src="https://unsplash.com/s/photos/'+width+'/'+height+'/desserts" alt="pretty kitty">';
+    allImages += '<img src="https://unsplash.com/s/photos/desserts'+width+'/s'+height+ '" alt="desserts">';
 }
 
 $('#photos').append(allImages);
