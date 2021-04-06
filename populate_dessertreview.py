2dc4229764ef3ef7e26f659eed59fde2dba0a868
Robbie Edgar <57593722+robedg@users.noreply.github.com>
2021-04-06 01:18:14 +0100

    Added to population script

--- populate_dessertreview.py
+++ populate_dessertreview.py
@@ -8,13 +8,27 @@ from dessertreview.models import Shop, Category, Dessert
 
 def populate():
     categories=["Pancakes", "Waffles", "Crepes", "Sundaes", "Milshakes"]
+    shops=[
+        ["Flavas", 55.86926982012218, -4.274004072128909],
+        ["Big Licks", 55.86211812556167, -4.280469284050763]
+    ]
+    desserts=[
+        ["Flavas", "Sugar Waffle", "Tasty and sweet", "Waffles"]
+    ]
     for cat in categories:
         add_cat(cat)
+
+    for shop in shops:
+        add_shop(shop[0],shop[1],shop[2])
 
 def add_cat(name):
     c=Category.objects.get_or_create(name=name)[0]
     c.save()
     return c
+
+def add_shop(name, lat, lng):
+    s=Shop.objects.get_or_create(name=name, lat=lat, lng=lng)[0]
+    s.save()
 
 if __name__ == '__main__':
     populate()
